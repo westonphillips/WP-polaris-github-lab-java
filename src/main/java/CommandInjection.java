fcce505f500fbd9a1536174795cd2a7fa778ae3a
Jason Phillips <pjason@blackduck.com>
2025-04-30 09:20:11 -0400

    Update CommandInjection.java

--- src/main/java/CommandInjection.java
+++ src/main/java/CommandInjection.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 public class CommandInjection {
     public static Process runCmd(HttpServletRequest request) throws IOException {
         String filename = request.getParameter("filename");
+        String secret = "QXBwbGljYXRpb24gU2VjdXJpdHk=";
         ProcessBuilder builder = new ProcessBuilder("cat", filename);
         Process process = builder.start();
         return(process);
